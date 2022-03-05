ff6c255f5f5f6243f90fc18d83823612b056319a
Tweetsauce <pobusauce12@gmail.com>
2022-03-05 14:03:29 +0400

    some configs baybee

---
+++ cmus/lib.pl
@@ -0,0 +1,4 @@
+/Users/Sia/Music/Chilli Beans_/d a n c i n g a l o n e/01 - Chilli Beans_ - lemonade.ogg
+/Users/Sia/Music/Chilli Beans_/d a n c i n g a l o n e/02 - Chilli Beans_ - See C Love.ogg
+/Users/Sia/Music/Chilli Beans_/d a n c i n g a l o n e/03 - Chilli Beans_ - シェキララ.ogg
+/Users/Sia/Music/Chilli Beans_/d a n c i n g a l o n e/04 - Chilli Beans_ - Digital Persona.ogg
---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+
+fish_vi_key_bindings
+
+fish_add_path /usr/local/bin #new
+fish_add_path /usr/local/sbin
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/Sia/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+export TERM=xterm-256color
+export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*}"'
+export FZF_DEFAULT_OPTS='-m --height 50% --border'
+# ~/.config/fish/config.fish
+starship init fish | source
+
+status --is-interactive; and rbenv init - fish | source
