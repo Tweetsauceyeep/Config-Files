68d7bd538afcfa5d998c4088d377c3d9f5063c74
Tweetsauce <pobusauce12@gmail.com>
2022-07-19 15:56:00 +0800

    my bor

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -2,4 +2,5 @@
 
 #sketchybar --set $NAME label="$(date '+|   %H:%M:%S |   %m/%d/%y (%a) | ')"
 
-sketchybar --set $NAME label="$(date '+ (%a) %m/%d |  %H:%M:%S |')"
+#sketchybar --set $NAME label="$(date '+ (%a) %m/%d |  %H:%M:%S |')"
+sketchybar --set $NAME label="$(date '+ %m-%d-%Y | %I:%M:%S |')"
